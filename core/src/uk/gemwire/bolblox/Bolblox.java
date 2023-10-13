6b3648f50ada15a848ce3fa9e1a95f853d5665fc
Curle <curle@gemwire.uk>
2023-10-13 22:12:03 +0100

    Initial commit - GDX

---
+++ core/src/uk/gemwire/bolblox/Bolblox.java
@@ -0,0 +1,22 @@
+package uk.gemwire.bolblox;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Bolblox extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
---
+++ html/src/uk/gemwire/bolblox/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package uk.gemwire.bolblox.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import uk.gemwire.bolblox.Bolblox;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Bolblox();
+        }
+}
